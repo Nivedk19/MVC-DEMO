13db3d9965a69a4cc6f65c237ae398b2af6888a7
NIVED K KUPPADAKKATH <nivedk.kuppadakkath@dxc.com>
2020-09-08 22:41:20 +0530

    first command

---
+++ casestudy/casestudy/Models/user.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace casestudy.Models
+{
+    public class user
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        [Display(Name = "First Name")]
+        public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [Display(Name = "First Name")]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(maximumLength: 8, ErrorMessage = "password length should be atleast 8 characters")]
+        [RegularExpression("(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{8,15})$")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string Confirmpassword { get; set; }
+
+        [Display(Name = "Address")]
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+
+        [Display(Name = "Contact Number")]
+        [Required(ErrorMessage = "Contact number is required")]
+        public string Contactnumber { get; set; }
+
+        [Required(ErrorMessage = "email is required")]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "City")]
+        [Required(ErrorMessage = "City is required")]
+        public string City { get; set; }
+
+        [Display(Name = "Country")]
+        [Required(ErrorMessage = "Country is required")]
+        public string Countryy { get; set; }
+
+        public string Role { get; set; }
+    }
+}
---
+++ casestudy/casestudy/Models/usercontext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace casestudy.Models
+{
+    public class usercontext:DbContext
+    {
+        public usercontext() : base("name=Dbconfig")
+        {
+
+
+
+
+        }
+        public DbSet<user> userdetails { get; set; }
+    }
+}
